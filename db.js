15c2e7534ee92beb983fa113ea31be145ee35455
Deacyn <cheng.zhixin@foxmail.com>
2018-09-12 15:19:40 +0800

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 var mysql = require('mysql');  
 var pool  = mysql.createPool({  
   connectionLimit : 10,  
-  host: 'rm-2zeph8t4c7q1w0g34o.mysql.rds.aliyuncs.com',//数据库地址
+  host: 'xxxx',//数据库地址
   user : "iwan",//数据库用户
   password: "zBgx6GdIW4dUsEMB",//数据库密码
   database : "h5_game_iwanhudong_cn"//需要连接的数据库
